0c7b9a6125cdd5fc9fc2650d7bd1043fc048889f
shreykoradia <shrey.koradia@yahoo.com>
2021-08-21 17:10:52 +0530

    routes param are set only left is to work towards nodemailer

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+// const { parseConnectionUrl } = require('nodemailer/lib/shared');
+
 
 // setting up the view engine for rendering on the screen 
 app.set('view engine', 'ejs');
 
+// setting up the body parser
+app.use(express.urlencoded({extended: true}))
+//  setting the parser
+app.use(express.json());
+
 // setting the routes 
 
 app.get('/' , (req , res)=>{
-
    res.render('index.ejs')
-
 })
 
+app.post('/send' , (req , res)=>{
+
+ const output = ` <p>You have a New Mail via Email-Sender </p>
+                    <h3>Mail Details : </h3>
+                    <ul>E-Mail :${req.body.mail}</ul> 
+                    <h3>Message : </h3>
+                    <p>${req.body.message}</p>`;
+
+})
 
 
 // setting up the server code 
