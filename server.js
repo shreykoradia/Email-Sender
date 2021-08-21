e8dc83aa988b87e5c374c57a8f029a1684a40714
shreykoradia <shrey.koradia@yahoo.com>
2021-08-21 17:52:55 +0530

    the project is finished and is not able to work

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
 require('dotenv').config
 // const { parseConnectionUrl } = require('nodemailer/lib/shared');
+
 
 
 // setting up the view engine for rendering on the screen 
@@ -12,6 +14,11 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
 //  setting the parser
 app.use(express.json());
+
+// var 
+const mail = process.env.mail;
+const password = process.env.password;
+
 
 // setting the routes 
 
@@ -23,9 +30,44 @@ app.post('/send' , (req , res)=>{
 
  const output = ` <p>You have a New Mail via Email-Sender </p>
                     <h3>Mail Details : </h3>
-                    <ul>E-Mail :${req.body.mail}</ul> 
+                    <ul>E-Mail :${req.body.email}</ul> 
                     <h3>Message : </h3>
                     <p>${req.body.message}</p>`;
+
+        
+                    // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: mail ,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: mail , // generated ethereal user
+        pass: password // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: mail , // sender address
+      to: `${req.body.email}` , // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello Friend ', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      
+  });
 
 })
 
